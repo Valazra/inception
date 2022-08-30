56233d850a19c638035c13a213352d9bdad891eb
Azra <valentin.azra@gmail.com>
2022-08-30 11:51:26 +0200

    Base solide mais avec des latest et des links

--- srcs/requirements/nginx/nginx.sh
+++
@@ -1,8 +0,0 @@
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=vazra/CN=localhost"
-
-mkdir var/www/localhost
-mv ./default etc/nginx/sites-available
-ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
-chown -R www.data /var/www/*
-chmod -R 755 /var/www/*
--- srcs/requirements/wordpress/wordpress.sh
+++
@@ -1,3 +0,0 @@
-chown -R 755 /var/www/*;
-mkdir -p /run/php/;
-touch /run/php/php7.3-fpm.pid;
