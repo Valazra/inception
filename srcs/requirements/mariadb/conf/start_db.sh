a5a6598b633989dcf1552036a497140ca9975222
vazra <valentin.azra@gmail.com>
2022-10-03 01:33:29 +0200

    on change des minis trucs mais ca marche tjr pas

--- srcs/requirements/mariadb/conf/start_db.sh
+++ srcs/requirements/mariadb/conf/start_db.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS 'wordpress';"
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mysql -e "CREATE USER IF NOT EXISTS '$LOGIN' IDENTIFIED BY '$PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON 'wordpress'.* TO '$LOGIN'@'%' IDENTIFIED BY '$PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$LOGIN'@'%' IDENTIFIED BY '$PASSWORD';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
 
